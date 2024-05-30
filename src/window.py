9caaa2f00cbd3ea3a39873840985d1a2ef872e2c
Jordi <92721410+JordiHerrera@users.noreply.github.com>
2024-05-30 22:39:31 +0200

    Update window.py

--- src/window.py
+++ src/window.py
@@ -1,22 +1,23 @@
 import eel
 import main as mn
 
+# Initialize Eel
 eel.init('web')
 eel._exposed_functions = {}
 
 
 def u():
-    # eel.expose(scripts, record_and_upload, wait_for_new_file, download_file, delete_file)
     mn.record_and_upload("audio")
     bucket_name = 'audio-script-sm'
     destination = 'to_script.json'
     blob = mn.wait_for_new_file(bucket_name)
     mn.download_file(bucket_name, blob.name, destination)
     mn.delete_file(bucket_name, blob.name)
-    # mn.print_message("Scripts!")
-    mn.scripts()
-    u()
+    if (mn.scripts() == 1):
+        u()
 
-if __name__ =='__main__':
-    eel.expose(u)
-    eel.start('index.html', size=(1000, 900))
+
+print("Exposed functions after:", eel._exposed_functions)
+eel.expose(u)
+print("Exposed functions after:", eel._exposed_functions)
+eel.start('index.html', size=(1200, 1000))
